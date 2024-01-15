eedf3bd4c5f1b4b2649c1121fff7edf0a2d653ce
mayozesoup <106310940+mayozesoup@users.noreply.github.com>
2024-01-15 09:20:26 +0700

    Update sketch.js
    Add in ground and circle

--- sketch.js
+++ sketch.js
@@ -1,1 +1,18 @@
+let y, direction;
+function draw_tree(){
+  
+}
+function setup() {
+  createCanvas(800, 500);
+  y = 200;
+  direction = true;
+}
 
+function draw() {
+  background(220);
+  fill('green')
+  rect(0,350,800,150);
+  fill('gray')
+  ellipse(100, y, 100, 100);
+  
+}
