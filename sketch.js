f73d0cad6af5bb7003ee1d6bf0ee9442a2676366
mayozesoup <106310940+mayozesoup@users.noreply.github.com>
2024-01-23 10:28:39 +0700

    Added Draw Tree function
    Draw trees draws a tree at the desired location with a certain size

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 let y, direction;
-function draw_tree(){
-  
+function draw_tree(x,y,size){
+  fill('brown');
+  rect(x-(size/4),y,size/2,size)
+  fill('green')
+  triangle(x-size,y,x,y-size*1.5,x+size,y)
+  size = size*0.8
+  triangle(x-size,y-size,x,y-size*2.5,x+size,y-size)
+  size = size*0.8
+  triangle(x-size,y-size/0.8*2,x,y-size*2.5-size/0.8,x+size,y-size/0.8*2)
 }
 function setup() {
   createCanvas(800, 500);
@@ -10,9 +17,10 @@ function setup() {
 
 function draw() {
   background(220);
-  fill('green')
+  fill('green');
   rect(0,350,800,150);
-  fill('gray')
+  fill('gray');
   ellipse(100, y, 100, 100);
+  draw_tree(400,250,100);
   
 }
